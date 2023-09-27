e27d34d2ffd32b42ab0a890613149520010e3ef1
Lainezz <diegolinaresmojacar@gmail.com>
2023-09-27 14:02:44 +0200

    adicion codigo clase

---
+++ src/com/es/programacion/tema1/TeoriaTema1B.java
@@ -0,0 +1,16 @@
+package com.es.programacion.tema1;
+
+public class TeoriaTema1B {
+
+    public static void main(String[] args) {
+
+        /*
+        Vamos a hacer un ejercicio que, dado un número que el usuario introduce por teclado, decir si ese número
+        es positivo o negativo
+         */
+
+
+
+
+    }
+}
