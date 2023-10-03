b9172e93b9244981c1c389e719e40c214f2e30c9
Lainezz <diegolinaresmojacar@gmail.com>
2023-10-03 09:27:05 +0200

    Adicion codigo actividad

--- src/com/es/programacion/tema2/ActividadClase.java
+++ src/com/es/programacion/tema2/ActividadClase.java
@@ -4,5 +4,34 @@ public class ActividadClase {
 
     public static void main(String[] args) {
 
+        /**
+         * DOS PROGRAMAS. Debéis hacer uso del bucle correcto.
+         * 1. Hacer un programa que pida números y los muestre por pantalla
+         * hasta que el usuario introduzca un número negativo.
+         *
+         * EJEMPLO
+         * -> introduce 5
+         * -> muestro "Numero introducido: 5"
+         * -> introduce -1 (o cualquier otro num negativo)
+         * -> muestra "Adiosito"
+         *
+         * 2. Hacer un programa que pida un número (positivo) y muestre los
+         * valores pares desde 0 hasta ese número.
+         * Si el usuario no introduce un número correcto, el programa debe seguir pidiéndole
+         * que introduzca un número correcto.
+         *
+         * EJEMPLO
+         * -> Programa pide un numero
+         * -> Usuario introduce -> -5
+         * -> Programa muestra -> Error, pruebe de nuevo
+         * -> Usuario introduce -> -10
+         * -> Programa muestra -> Error, pruebe de nuevo
+         * -> Usuario introduce -> 4
+         * -> Programa muestra -> 0, 2, 4
+         * -> Fin
+         *
+         */
+
+
     }
 }
