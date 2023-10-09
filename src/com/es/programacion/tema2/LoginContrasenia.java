4e6c9dcce05bf0d7f6bd4663ac05e94b64a0f308
Lainezz <diegolinaresmojacar@gmail.com>
2023-10-09 14:40:27 +0200

    adicion codigo clase

--- src/com/es/programacion/tema2/LoginContrasenia.java
+++ src/com/es/programacion/tema2/LoginContrasenia.java
@@ -1,5 +1,7 @@
 package com.es.programacion.tema2;
 
+import java.util.Scanner;
+
 public class LoginContrasenia {
 
     public static void main(String[] args) {
@@ -7,18 +9,42 @@ public class LoginContrasenia {
          * Hacer un programa que lea una contraseña dada por el usuario
          * y que la compare con la contraseña en sí.
          *
-         * La contraseña es: "1234567890"
+         * La contraseña es: "pepe1234567890"
          *
          * Si esa contraseña coincide, se acepta el acceso
          * Si no coincide, poned un mensaje "Acceso denegado"
          */
 
-        // 1º declarar variables contrasenia y cadena/intento/try
+        // 1º declarar variables contrasenia y user
+        String usuarioSis = "admin";
+        String intentoUser = "";
+
+        String contraseniaSis = "pepe1234567890";
+        String intentoPassw = "";
 
         // 2º Leer por teclado lo que introduce el user
+        Scanner scan = new Scanner(System.in);
 
-        // 3º Compararlo con la contrasenia del sistema
+        do {
 
-        // 4º Mostrar el mensaje
+            System.out.print("Introduzca su usuario:");
+            intentoUser = scan.next();
+
+            System.out.print("Introduzca la password: ");
+            intentoPassw = scan.next();
+
+            if(!usuarioSis.equals(intentoUser) || !contraseniaSis.equals(intentoPassw)){
+                System.out.println("Credenciales incorrectas!! Intentalo de nuevo");
+            }
+
+        } while(!usuarioSis.equals(intentoUser) || !contraseniaSis.equals(intentoPassw));
+
+
+
+
+        // Cuando sale del while, significa que se han introducido las credenciales correctas
+        System.out.print("Bienvenid@ "+intentoUser);
+
+
     }
 }
