6598a560006b4d6c88c45ec40ca6d41ebcaf4df0
Lainezz <diegolinaresmojacar@gmail.com>
2023-10-09 14:01:04 +0200

    codigo de clase

---
+++ src/com/es/programacion/tema2/LoginContrasenia.java
@@ -0,0 +1,24 @@
+package com.es.programacion.tema2;
+
+public class LoginContrasenia {
+
+    public static void main(String[] args) {
+        /**
+         * Hacer un programa que lea una contraseña dada por el usuario
+         * y que la compare con la contraseña en sí.
+         *
+         * La contraseña es: "1234567890"
+         *
+         * Si esa contraseña coincide, se acepta el acceso
+         * Si no coincide, poned un mensaje "Acceso denegado"
+         */
+
+        // 1º declarar variables contrasenia y cadena/intento/try
+
+        // 2º Leer por teclado lo que introduce el user
+
+        // 3º Compararlo con la contrasenia del sistema
+
+        // 4º Mostrar el mensaje
+    }
+}
