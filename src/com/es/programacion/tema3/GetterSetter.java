e6e5012025702887ceeefe93fc7c79a63ef6af32
Lainezz <diegolinaresmojacar@gmail.com>
2023-11-03 20:13:20 +0100

    resolucion de login mejorado

---
+++ src/com/es/programacion/tema3/GetterSetter.java
@@ -0,0 +1,30 @@
+package com.es.programacion.tema3;
+
+public class GetterSetter {
+
+    public static void main(String[] args) {
+
+        /**
+         * Implementa un programa donde un usuario debe rellenar
+         * un array de int de tamanio 10.
+         *
+         * Se ha de controlar mediante el uso de excepciones que no se introduzcan
+         * más de 10 elementos.
+         * **** Ayuda****
+         * Hacéis un bucle while... do while ... para que el user vaya introduciendo
+         * los datos.
+         * Cuando se intente introducir un dato en una posición que no exista... pues controláis
+         * esa excepción.
+         *
+         * Debéis implementar una función que compruebe que el número introducido
+         * es positivo. Si NO ES POSITIVO... lanza una excepción.
+         * **** Ayuda****
+         * Una función que recibe 1 número entero por parámetros.
+         * NO DEVUELVE NADA.
+         * La función se llama comprobar
+         *
+         */
+    }
+
+
+}
