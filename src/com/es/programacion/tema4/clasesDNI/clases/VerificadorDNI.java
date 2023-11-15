f69f02dabae3842232d67b96e55a6e0b85539101
Lainezz <diegolinaresmojacar@gmail.com>
2023-11-15 13:57:33 +0100

    adicion de enunciado

--- src/com/es/programacion/tema4/clasesDNI/clases/VerificadorDNI.java
+++ src/com/es/programacion/tema4/clasesDNI/clases/VerificadorDNI.java
@@ -3,6 +3,7 @@ package com.es.programacion.tema4.clasesDNI.clases;
 public class VerificadorDNI {
 
     private static String dni = "";
+    private static char[] letrasDNI = {};
 
     /**
      * Metodo que comprueba que la ultima posicion es una letra
@@ -30,7 +31,17 @@ public class VerificadorDNI {
     } // FIN comprobarLongitud
 
 
+    /**
+     * La letra asociada a un DNI se obtiene dividiendo el número del DNI entre 23 y el resto es codificado siguiendo la siguiente correspondencia:
+     * 0: "T", 1: "R", 2: "W", 3: "A", 4: "G", 5: "M", 6: "Y", 7: "F", 8: "P", 9: "D",10:"X", 11: "B", 12: "N",
+     * 13: "J", 14: "Z", 15: "S", 16: "Q", 17: "V", 18: "H", 19: "L", 20: "C", 21: "K", 22: "E"
+     * Un método que dado un DNI (con su letra), devuelva true o false si el DNI es válido o no.
+     * @param dni (String) dni a comprobar
+     * @return true si la letra es correcta
+     */
     public static boolean comprobarLetraCorrecta(String dni) {
+
+
         return true;
     }// FIN COMPROBARLetraCorrecta
 
