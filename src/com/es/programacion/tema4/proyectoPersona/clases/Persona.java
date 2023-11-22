5d8436426ab4a203b58d275c5b575cede02291ad
Lainezz <diegolinaresmojacar@gmail.com>
2023-11-22 14:23:30 +0100

    adicion codigo clase

---
+++ src/com/es/programacion/tema4/proyectoPersona/clases/Persona.java
@@ -0,0 +1,35 @@
+package com.es.programacion.tema4.proyectoPersona.clases;
+
+public class Persona {
+
+    // ATRIBUTOS DE CLASE
+    String nombre, dni, apellidos, colorPiel, colorPelo;
+    int edad;
+    double altura;
+
+    // METODOS DE CLASE
+
+    /**
+     * Método para presentar a una persona
+     */
+    public void presentarse() {
+        System.out.println("" +
+                "\nHola, mi nombre es "+this.nombre+" " +
+                "\ny tengo "+this.edad+" anios.");
+    }
+
+    /**
+     * Método que anade 1 anio a la edad
+     */
+    public void cumplirAnios() {
+        this.edad = this.edad + 1; // this.edad++; this.edad += 1;
+    }
+
+    public void cambiarColorPelo(String nuevoColorPelo) {
+        System.out.println("Tenia el pelo de color "+this.colorPelo);
+        this.colorPelo = nuevoColorPelo;
+        System.out.println("Y ahora lo tengo "+this.colorPelo);
+    }
+
+
+}
