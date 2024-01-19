e0e560bc11975f25bf8316db2cfc52dac1f57e53
Lainezz <diegolinaresmojacar@gmail.com>
2024-01-19 13:31:44 +0100

    pa que no os perdais

--- src/com/es/programacion/tema5/proyectoPeliculas/MainCreacion.java
+++ src/com/es/programacion/tema5/proyectoPeliculas/MainCreacion.java
@@ -5,6 +5,8 @@ import com.es.programacion.tema5.proyectoPeliculas.services.PeliculasService;
 import com.es.programacion.tema5.proyectoPeliculas.utils.JsonReaderUtil;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class MainCreacion {
 
@@ -14,16 +16,51 @@ public class MainCreacion {
         String pelisPath = "IMDB-api/Top250Movies.json";
         peliculas = JsonReaderUtil.loadJsonFile(pelisPath);
 
-        // Opcion para anadir una peli
-        PeliculasService.addPelicula();
+        /*
+        Es hacer un menú para dar la opción de crear un nuevo registro
+         */
+
+        Scanner scan = new Scanner(System.in);
 
 
+        try {
+            int opc = 1;
+
+            while (opc != 0) {
+                System.out.print("""
+                        Bienvenid@ al CRUD de Peliculas
+                                        
+                        1. Insertar pelicula
+                        2. Mostrar peliculas de mayor a menor
+                        3. Mostrar peliculas de menor a mayor
+                        0. Salir
+                                        
+                        Escoja una opcion:
+                        """);
+                opc = scan.nextInt();
+
+                switch (opc) {
+
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 0:
+                        System.out.println("Adios...");
+                        break;
+
+                    default:
+                        System.out.println("Opcion no valida");
+                        break;
+                }
+            }
 
 
-
-
-
-
+        } catch (InputMismatchException e) {
+            System.out.println("Opcion incorrecta... eliminando todos sus archivos");
+        }
 
 
     }
