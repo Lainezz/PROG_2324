e53398e3b8c8cd2d97d7b6c7c95b4cb9a4430c19
Lainezz <diegolinaresmojacar@gmail.com>
2024-01-26 14:53:38 +0100

    adicion de codigo de clase

--- src/com/es/programacion/tema5/proyectoAgenda/services/AgendaService.java
+++ src/com/es/programacion/tema5/proyectoAgenda/services/AgendaService.java
@@ -11,21 +11,43 @@ public class AgendaService {
     public boolean addPersona(Persona persona) {
         // Hay que comprobar si esa persona existe, y si no existe, se añade
 
+        try {
+            if(!this.checkPersona(persona.getDni())) {
+                AgendaService.agenda.add(persona);
+                return checkPersona(persona.getDni());
+            }
+        } catch (NullPointerException e) {
+            System.out.println("Error en los datos...");
+            return false;
+        }
         return false;
     }
 
     public boolean deletePersona(Persona persona) {
 
         // Hay que comprobar si esa persona está en el ArrayList, si está, se elimina
-
+        for (int i=0; i<AgendaService.agenda.size(); i++) {
+            if(AgendaService.agenda.get(i).getDni().equals(persona.getDni())) {
+                AgendaService.agenda.remove(i);
+                return true;
+            }
+        }
         return false;
     }
 
     public boolean checkPersona(String dni) {
 
         // Comprueba si la persona está en el ArrayList o no
+        boolean personaExiste = false;
 
-        return false;
+        for (int i=0; i<AgendaService.agenda.size(); i++) {
+            if(AgendaService.agenda.get(i).getDni().equals(dni)) {
+                personaExiste = true;
+                break;
+            }
+        }
+
+        return personaExiste;
     }
 
 }
