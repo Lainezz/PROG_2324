1793fda2fbe08b7fa45fc9cd8690dffcbc99d27b
Lainezz <diegolinaresmojacar@gmail.com>
2024-01-26 14:25:40 +0100

    adicion de codigo de clase

---
+++ src/com/es/programacion/tema5/proyectoAgenda/services/AgendaService.java
@@ -0,0 +1,31 @@
+package com.es.programacion.tema5.proyectoAgenda.services;
+
+import com.es.programacion.tema5.proyectoAgenda.clases.Persona;
+
+import java.util.ArrayList;
+
+public class AgendaService {
+
+    private static ArrayList<Persona> agenda = new ArrayList<Persona>();
+
+    public boolean addPersona(Persona persona) {
+        // Hay que comprobar si esa persona existe, y si no existe, se añade
+
+        return false;
+    }
+
+    public boolean deletePersona(Persona persona) {
+
+        // Hay que comprobar si esa persona está en el ArrayList, si está, se elimina
+
+        return false;
+    }
+
+    public boolean checkPersona(String dni) {
+
+        // Comprueba si la persona está en el ArrayList o no
+
+        return false;
+    }
+
+}
