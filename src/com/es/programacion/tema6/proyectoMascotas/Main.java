08b28c1ba5eae62bacb292e18f24584e91fc5dca
Lainezz <diegolinaresmojacar@gmail.com>
2024-02-12 14:48:02 +0100

    adicion de codigo de clase

--- src/com/es/programacion/tema6/proyectoMascotas/Main.java
+++ src/com/es/programacion/tema6/proyectoMascotas/Main.java
@@ -1,9 +1,29 @@
 package com.es.programacion.tema6.proyectoMascotas;
 
+import com.es.programacion.tema6.proyectoMascotas.clases.Gato;
+import com.es.programacion.tema6.proyectoMascotas.clases.Inventario;
+import com.es.programacion.tema6.proyectoMascotas.clases.Loro;
+import com.es.programacion.tema6.proyectoMascotas.clases.Perro;
+
 public class Main {
 
 
     public static void main(String[] args) {
+
+        Perro oreo = new Perro("Oreo", "Schnauzer", false);
+        Perro goku = new Perro("Goku", "Yorkshire", false);
+        Gato pulga = new Gato("Pulga", "Marron", false);
+        Loro pirri = new Loro("Pirri", "Granívoro", true, "Sudamérica");
+
+        Inventario invent = new Inventario();
+
+        invent.anadirMascota(oreo);
+        invent.anadirMascota(goku);
+        invent.anadirMascota(pulga);
+        invent.anadirMascota(pirri);
+
+        invent.mostrarDatosAnimales();
+
 
     }
 
