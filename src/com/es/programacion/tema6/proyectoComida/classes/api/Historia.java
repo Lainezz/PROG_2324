96d8468a64c766ffda0b377204cf693252a7654f
Lainezz <diegolinaresmojacar@gmail.com>
2024-02-14 16:50:53 +0100

    adicion de arreglo

--- src/com/es/programacion/tema6/proyectoComida/classes/api/Historia.java
+++ src/com/es/programacion/tema6/proyectoComida/classes/api/Historia.java
@@ -3,4 +3,10 @@ package com.es.programacion.tema6.proyectoComida.classes.api;
 public interface Historia {
 
     public void contarHistoria();
+
+    public void queHoraEs(int hora);
+
+    public void muertoCansancio(int hora);
+
+
 }
