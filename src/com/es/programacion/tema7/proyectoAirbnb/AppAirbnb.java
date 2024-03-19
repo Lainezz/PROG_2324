23e4dd112903683840651f604d477feff3ebd5c7
Lainezz <diegolinaresmojacar@gmail.com>
2024-03-19 12:26:08 +0100

    adicion de codigo clase

--- src/com/es/programacion/tema7/proyectoAirbnb/AppAirbnb.java
+++ src/com/es/programacion/tema7/proyectoAirbnb/AppAirbnb.java
@@ -1,6 +1,9 @@
 package com.es.programacion.tema7.proyectoAirbnb;
 
+import com.es.programacion.tema7.proyectoAirbnb.model.Alojamiento;
 import com.es.programacion.tema7.proyectoAirbnb.services.GestionFicheroAirbnb;
+
+import java.util.ArrayList;
 
 public class AppAirbnb {
 
@@ -10,8 +13,14 @@ public class AppAirbnb {
 
         String ruta = "src/main/resources/archivosTema7/airbnb/malaga.csv";
 
-        gestion.leerFicheroAlojamientos(ruta);
+        ArrayList<Alojamiento> alojamientosTemporal = new ArrayList<>();
 
+        alojamientosTemporal = gestion.leerFicheroAlojamientos(ruta);
+
+
+        for (Alojamiento a : alojamientosTemporal) {
+            System.out.println(a);
+        }
 
     }
 }
--- src/com/es/programacion/tema7/proyectoAirbnb/services/GestionFicheroAirbnb.java
+++ src/com/es/programacion/tema7/proyectoAirbnb/services/GestionFicheroAirbnb.java
@@ -1,8 +1,12 @@
 package com.es.programacion.tema7.proyectoAirbnb.services;
 
 import com.es.programacion.tema7.proyectoAirbnb.model.Alojamiento;
+import com.es.programacion.tema7.proyectoAirbnb.model.Propietario;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class GestionFicheroAirbnb {
@@ -15,8 +19,66 @@ public class GestionFicheroAirbnb {
 
     public ArrayList<Alojamiento> leerFicheroAlojamientos(String ruta) {
 
-        System.out.println("RUTA: "+ruta);
-        //File fichero = new File(ruta);
-        return  null;
+        ArrayList<Alojamiento> alojamientosTemporal = new ArrayList<>();
+
+        System.out.println("RUTA: " + ruta);
+        File fichero = new File(ruta);
+
+        if (fichero.exists()) {
+
+            FileReader fr = null;
+            BufferedReader br = null;
+
+            try {
+                fr = new FileReader(fichero);
+                br = new BufferedReader(fr);
+
+                String linea = br.readLine();
+                int i = 0;
+                while (linea != null) {
+                    if (i == 0) {
+                        i++;
+                    } else {
+
+                        String id = "";
+                        String name = "";
+                        String host_id = "";
+                        String host_name = "";
+                        String price = "";
+
+                        String[] valores = linea.split(",");
+
+                        id = valores[0];
+                        name = valores[1];
+                        host_id = valores[2];
+                        host_name = valores[3];
+                        price = valores[9];
+
+                        double price_d = 0;
+                        try {
+                            price_d = Double.parseDouble(price);
+                        } catch (NumberFormatException e) {
+                            e.printStackTrace();
+                        }
+
+                        Propietario p = new Propietario(host_id, host_name);
+                        Alojamiento a = new Alojamiento(id, name, p, price_d);
+
+                        alojamientosTemporal.add(a);
+
+                    }
+
+                    linea = br.readLine();
+
+
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        return alojamientosTemporal;
     }
 }
