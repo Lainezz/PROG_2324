425b04076952bb3480742dc0643f7dc1a2c98d42
Lainezz <diegolinaresmojacar@gmail.com>
2024-04-16 10:09:04 +0200

    adicion de codigo clase

---
+++ src/com/es/programacion/tema8/teoriaLayouts/layoutBorder/AppBorder.java
@@ -0,0 +1,10 @@
+package com.es.programacion.tema8.teoriaLayouts.layoutBorder;
+
+public class AppBorder {
+
+    public static void main(String[] args) {
+
+        FrameBorder ventana = new FrameBorder();
+
+    }
+}
