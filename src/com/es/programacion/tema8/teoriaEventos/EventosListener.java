a3e74a96f46fedaa15f11dcc9e04bc11fa7d41aa
Lainezz <diegolinaresmojacar@gmail.com>
2024-04-22 14:53:39 +0200

    adicion de codigo clase

--- src/com/es/programacion/tema8/teoriaEventos/EventosListener.java
+++ src/com/es/programacion/tema8/teoriaEventos/EventosListener.java
@@ -2,8 +2,11 @@ package com.es.programacion.tema8.teoriaEventos;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.InputMismatchException;
 
 public class EventosListener extends JFrame {
 
@@ -13,6 +16,7 @@ public class EventosListener extends JFrame {
     // Atributos de clase
     JButton botonEnviar;
     JTextField campoTexto;
+    JLabel resultado;
 
     // Creamos los eventos
     MouseListener listenerDeBotonEnviar = new MouseListener() {
@@ -20,6 +24,23 @@ public class EventosListener extends JFrame {
         public void mouseClicked(MouseEvent e) {
             System.out.println("Boton pulsado");
             String texto = campoTexto.getText();
+
+            // Debe salir un mensaje de si el numero es par o impar
+            try {
+                long num = Integer.parseInt(texto);
+                if (num%2 == 0) {
+                    System.out.println("Es par");
+                    resultado.setText("Es Par");
+                } else {
+                    System.out.println("Es impar");
+                    resultado.setText("Es Impar");
+                }
+                campoTexto.setBackground(new Color(0, 0, 0));
+
+            } catch (NumberFormatException ex) {
+                campoTexto.setBackground(new Color(255, 0, 0));
+                resultado.setText("Error!");
+            }
 
             System.out.println(texto);
         }
@@ -47,6 +68,25 @@ public class EventosListener extends JFrame {
         }
     };
 
+    KeyListener listenerTeclado = new KeyListener() {
+        @Override
+        public void keyTyped(KeyEvent e) { //
+            if(e.getKeyChar() == KeyEvent.VK_ENTER) {
+                System.out.println("Intro pulsado");
+            }
+
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+
+        }
+    };
 
 
     // Constructor de clase
@@ -71,16 +111,22 @@ public class EventosListener extends JFrame {
         campoTexto.setBackground(new Color(0,0,0));
         campoTexto.setForeground(new Color(255,255,255));
         campoTexto.setFont(new Font("Consolas", Font.BOLD, 16));
+        campoTexto.addKeyListener(listenerTeclado);
 
         botonEnviar = new JButton("Enviar");
         botonEnviar.setPreferredSize(new Dimension(100,32));
         // Anadimos el evento al botonEnviar
         botonEnviar.addMouseListener(listenerDeBotonEnviar);
 
+        resultado = new JLabel();
+        resultado.setFont(new Font("Consolas", Font.BOLD, 32));
+
+
 
         // Anadimos elementos al JPanel (panelCentro)
         panelCentro.add(campoTexto);
         panelCentro.add(botonEnviar);
+        panelCentro.add(resultado);
 
         // Anadimos elementos al JFrame
         this.add(panelCentro, BorderLayout.CENTER);
